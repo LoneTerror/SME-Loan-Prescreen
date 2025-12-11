6047c1b25686d8c4f6a051fa62fbee418513a923
LoneTerror <nothingshere21@gmail.com>
2025-12-11 12:26:22 +0530

    Allowed Hosts Updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ["powersense.site"],
+    allowedHosts: ["spot-check.site"],
   },
 })
